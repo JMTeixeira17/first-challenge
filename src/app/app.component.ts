264e3a1ebcc4252b79260c101be58ed94977629c
sChemaPandaID <jmteixeira@pandaid.com>
2024-02-16 16:28:53 -0400

    Add: Balance Wallet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
-
+import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
+import { ShyftApiService } from './shyft-api.service';
+import { WalletStore } from '@heavy-duty/wallet-adapter';
+import { computedAsync } from 'ngxtension/computed-async';
+import { toSignal } from '@angular/core/rxjs-interop'
+import { MatAnchor } from '@angular/material/button'
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, HdWalletMultiButtonComponent, MatAnchor],
   selector: 'first-challenge-root',
-  templateUrl: './app.component.html',
+  template: `
+  <header class="px-16 pt-24 pb-8">
+    <h1 class="text-center text-5xl mb-4"> Hola, soy Chema :D</h1>
+    <div class="flex justify-center mb-4">
+      <hd-wallet-multi-button></hd-wallet-multi-button>
+    </div>
+
+    @if(account()){
+      <div class="absolute top-4 left-4 flex items-center gap-2">
+        <img [src]="account()?.info?.image" class="w-8 h-8"/>
+      <p class="text-2xl font-bold">
+        {{account()?.balance}}
+      </p>
+      </div>
+    }
+    </header>
+
+    <nav>
+      <ul class="flex justify-center items-center gap-4">
+        <li>
+          <a [routerLink]="['']" mat-raised-button>Home</a>
+        </li>
+        <li>
+          <a [routerLink]="['settings']" mat-raised-button >Settings</a>
+        </li>
+      </ul>
+    </nav>
+
+    <main>
+      <router-outlet></router-outlet>
+    </main>
+  `,
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'first-challenge';
+  private readonly _shyftApiService = inject(ShyftApiService);
+  private readonly _walletStore = inject(WalletStore);
+  private readonly _publicKey = toSignal(this._walletStore.publicKey$)
+
+
+  readonly account = computedAsync(() => this._shyftApiService.getAccount(this._publicKey()?.toBase58()), { requireSync: true })
+
 }
+
+
+
---
+++ src/app/settings-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'settings-page',
+    template: `
+    <h2>Settings</h2>
+    `,
+    standalone: true
+})
+
+
+export class SettingsPageComponent{
+
+}
